275fff436ecc506326507b94e884e514e55e1af7
andyvy <thienvy95@gmail.com>
2023-04-04 23:34:53 +0700

    Basic Sidebar

---
+++ components/IconButton.tsx
@@ -0,0 +1,18 @@
+interface Props {
+	icon: React.ForwardRefExoticComponent<Omit<React.SVGProps<SVGSVGElement>, "ref"> & {
+        title?: string | undefined;
+        titleId?: string | undefined;
+    } & React.RefAttributes<SVGSVGElement>>
+	label: string
+}
+
+const IconButton = ({ icon: Icon, label }: Props) => {
+	return (
+		<button type="button" className='flex items-center space-x-2 hover:text-white'>
+			<Icon className='icon' />
+			<span>{label}</span>
+		</button>
+	)
+}
+
+export default IconButton
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
+import Center from '@/components/Center'
+import Sidebar from '@/components/Sidebar'
+import Head from 'next/head'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className='bg-black h-screen overflow-hidden'>
+      <Head>
+        <title>Spotify 2.0</title>
+        <meta name='description' content='Spotify by Andy' />
+      </Head>
+      <main className='flex'>
+        <Sidebar />
+        <Center />
+      </main>
     </div>
   )
 }
